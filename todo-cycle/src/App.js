6f157f63a84b2145a8aeb1451355a15504480af9
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-12-09 21:22:55 -0500

    added starter functions

---
+++ todo-cycle/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import TodoList from './todolist.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ todo-cycle/src/todolist.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Todo from './todo.js';
+
+class TodoList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todoNumber: 1
+        };
+        console.log("GreetingContrainer constructor");
+    }
+
+    renderTodo() {
+        const { todoNumber } = this.state;
+        let todoComponents = [];
+
+        for (let i = 0; i < todoNumber; i++) {
+            todoComponents.push(
+                <Todo key={i} id={String.fromCharCode(i + 65)} />
+            );
+        }
+        return todoComponents;
+    }
+
+    componentDidMount() {
+        console.log("TodoList mounted");
+    }
+
+    componentDidUpdate() {
+        console.log(
+            "TodoList updated: Now with " +
+            this.state.todoNumber +
+            " Todos"
+        );
+    }
+
+    addTodo = () => {
+        this.setState({
+            todoNumber: this.state.todoNumber + 1
+        });
+    };
+
+    removeTodo = () => {
+        this.setState({
+            todoNumber: Math.max(0, this.state.todoNumber - 1)
+        });
+    };
+
+    render() {
+        console.log("TodoList render");
+        return (
+            <div>
+                {this.renderTodo()}
+                <div>
+                    <button onClick={this.addTodo}>+</button>
+                    <button onClick={this.removeTodo}>-</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default TodoList;
